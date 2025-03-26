8bf0ae9fffda46e28ebbdfb58dcbb2300fee47d8
Yuan MA <mayuanmyra@gmail.com>
2025-03-26 20:45:55 +1100

    Read and print a xlsx file

--- excelDemo.js
+++ excelDemo.js
@@ -1,1 +1,17 @@
-const ExcelJS = require('exceljs');
+const ExcelJS = require('exceljs'); // import ExcelJS class from exceljs
+
+const workbook = new ExcelJS.Workbook();
+workbook.xlsx.readFile("D:\\Udemy\\ExcelJSUtil\\excelDownloadTest.xlsx").then(function(){
+    const worksheet = workbook.getWorksheet('Sheet1');// hold the data of Sheet1
+
+//read and print data in the worksheet
+worksheet.eachRow( (row, rowNumber) =>
+{
+    row.eachCell((cell, colNumber) =>
+    {
+        console.log(cell.value);
+    }
+    )
+})
+});
+
